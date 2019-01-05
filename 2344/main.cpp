ab1a8ef3eb277c23cbae74f6b84a1181d60e9b99
Andy Huynh L <andy.huynh7@outlook.com>
2019-01-04 20:38:27 -0500

    laptop version

--- 2344/main.cpp
+++ 2344/main.cpp
@@ -4,9 +4,6 @@ int main(int argc, char **argv)
 {
 	printf("hello world\n");
 	printf("hello worlds\n");
-	printf("hello worldz\n");
-	printf("hello worldz\n");
-	printf("hello worldz\n");
 	
 	return 0;
 }
